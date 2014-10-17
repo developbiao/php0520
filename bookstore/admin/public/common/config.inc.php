cf3769ffeed43b59f3a77d516dc7f23dcb276794
gongbiao <developbiao@gmail.com>
2014-10-17 15:39:00 +0800

    完成了在我的Ubuntu环境下布置代码配置数据库。

--- bookstore/admin/public/common/config.inc.php
+++ bookstore/admin/public/common/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-mysql_connect('192.168.176.128','root','123456');
+mysql_connect('localhost:3306','root','123456');
 mysql_select_db('bookshop');
 mysql_query('set names utf8');
 ?>
--- bookstore/home/public/common/config.inc.php
+++ bookstore/home/public/common/config.inc.php
@@ -1,5 +1,5 @@
 <?php 
-mysql_connect('192.168.176.128:3306','root','123456');
+mysql_connect('localhost:3306','root','123456');
 mysql_select_db('bookshop');
 mysql_query('set names utf8');
 ?>
